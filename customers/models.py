68debb1642af8feb97b843b497508c2fc4eff257
Aditya Sharma <121496528+AdityaSharma008@users.noreply.github.com>
2025-07-21 18:30:08 +0530

    define models for customers and loans

--- customers/models.py
+++ customers/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Customer(models.Model):
+   customer_id = models.AutoField(primary_key=True)
+   first_name = models.CharField(max_length=100, blank=False)
+   last_name = models.CharField(max_length=100, blank=False)
+   age = models.PositiveIntegerField()
+   phone_number = models.IntegerField()
+   monthly_salary = models.PositiveIntegerField()
+   approved_limit = models.PositiveIntegerField()
+   current_debt = models.PositiveIntegerField()
--- loans/models.py
+++ loans/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from customers.models import Customer
 
-# Create your models here.
+class Loan(models.Model):
+   loan_id = models.AutoField(primary_key=True)
+   customer = models.ForeignKey(Customer, on_delete=models.CASCADE, related_name="loans")
+   loan_amount = models.DecimalField(max_digits=12, decimal_places=2)
+   tenure = models.PositiveIntegerField()
+   interest_rate = models.DecimalField(max_digits=4, decimal_places=2)
+   monthly_repayment = models.DecimalField(max_digits=12, decimal_places=2)
+   emi_paid_on_time = models.PositiveIntegerField(default=0)
+   start_date = models.DateField()
+   end_date = models.DateField()
