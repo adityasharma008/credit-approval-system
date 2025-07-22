bb9dbe00fb9a529c1f95edfea6db2c26bd56ceb8
adityasharma008 <aditya.sharmaa008@gmail.com>
2025-07-22 14:55:01 +0530

    add models to admin panel

--- customers/admin.py
+++ customers/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Customer
 
-# Register your models here.
+@admin.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('customer_id', 'first_name', 'last_name', 'age', 'phone_number', 'monthly_salary')
+    search_fields = ('customer_id', 'first_name', 'last_name')
--- loans/admin.py
+++ loans/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Loan
 
-# Register your models here.
+@admin.register(Loan)
+class LoanAdmin(admin.ModelAdmin):
+    list_display = ('loan_id', 'customer_id', 'loan_amount', 'tenure', 'interest_rate', 'end_date')
+    search_fields = ('loan_id', 'customer__first_name', 'customer__last_name')
